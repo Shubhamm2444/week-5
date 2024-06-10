a81d9078c0c45977d5b0badba0b50d4988c7b8e9
shubham kumar <122029072+Shubhamm2444@users.noreply.github.com>
2024-06-10 16:16:47 +0530

    Create bash.js

---
+++ _46_lecture/bash.js
@@ -0,0 +1,9 @@
+1: Dependencies:
+
+Bash
+npm install express jsonwebtoken bcryptjs cors (optional)
+
+2: express: Web framework for building the server
+jsonwebtoken: Library for generating and verifying JWTs
+bcryptjs: Library for securely hashing passwords
+cors (optional): Library for enabling Cross-Origin Resource Sharing (CORS) if needed
