86854dd7710c9be9bf9b4f4661aa0c24bd43ce11
shubham kumar <122029072+Shubhamm2444@users.noreply.github.com>
2024-06-10 16:17:35 +0530

    Create (User Model)user.js

---
+++ _46_lecture/(User Model)user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs'); // Import bcrypt
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+// Hash password before saving a new user
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+module.exports = mongoose.model('User', UserSchema);
